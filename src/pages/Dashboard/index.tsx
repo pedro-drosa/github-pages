a4d3798c8a18ef579802eae2a45c878644b085bd
pedro-drosa <pedro_drosa@hotmail.com>
2021-03-02 20:51:09 -0300

    created: Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
-import { Title } from './style';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './style';
+
+import githubLogo from '../../assets/logo-github-explorer.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
-}
+  return (
+    <>
+      <img src= {githubLogo} alt='Github explorer'></img>
+      <Title>Explore repositórios no Github</Title>
+      <Form action="">
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="#">
+          <img 
+          src="https://avatars.githubusercontent.com/u/33488150?s=460&u=a88d34bff6d0faf455f301d2c2d64b025c7cd387&v=4"
+          alt="Pedro Juraci"
+          />
+          <div>
+            <strong>rocketseat/gobarber-backend</strong>
+            <p>Gostack Rocketseat - bootcamp project [ backend ]</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
